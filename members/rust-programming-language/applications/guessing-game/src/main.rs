906cb09477d38fa3e8c046cbd0b7907c2c630485
Michael Ahlers <michael@ahlers.consulting>
2024-01-08 15:44:58 -0700

    Make it functional

--- members/rust-programming-language/applications/guessing-game/src/main.rs
+++ members/rust-programming-language/applications/guessing-game/src/main.rs
@@ -1,15 +1,18 @@
-use std::io::stdin;
-use std::io::Result;
+use std::io;
+use std::io::BufRead;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    println!("Please input your guess:");
 
-    let mut guess: String = String::new();
-
-    let result: Result<usize> = stdin().read_line(&mut guess);
-    result.expect("Failed to read line.");
-
-    println!("You guessed: \"{guess}\".");
+    io::stdin()
+        .lock()
+        .lines()
+        .next()
+        .unwrap()
+        .map(|guess| {
+            println!("You guessed: \"{guess}\".");
+        })
+        .expect("Failed to read line.");
 }
