36ed562a6b103ed8280a6ac22bd84da7ff0ce0a8
Michael Ahlers <michael@ahlers.consulting>
2024-01-07 18:45:45 -0700

    From the example

--- members/rust-programming-language/applications/guessing-game/src/main.rs
+++ members/rust-programming-language/applications/guessing-game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::stdin;
+use std::io::Result;
+
 fn main() {
-    println!("guessing-game")
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess: String = String::new();
+
+    let result: Result<usize> = stdin().read_line(&mut guess);
+    result.expect("Failed to read line.");
+
+    println!("You guessed: \"{guess}\".");
 }
